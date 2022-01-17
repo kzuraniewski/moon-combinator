39664746a9c88288199bf84f2dfff547dee32355
nick-here <zuraniewski.karol@gmail.com>
2022-01-17 19:51:15 +0100

    signal iterator works for 'empty' unit types (pure genius)

--- aai/sync/signal_iterator.lua
+++ aai/sync/signal_iterator.lua
@@ -1,22 +1,24 @@
 -- desc: synchronic signal iterator
 -- FIXME: when no units of type, doesnt work
+local SIGNAL_NAMES = {
+    'vehicle-hauler-0-_-signal', 'vehicle-miner-0-_-signal',
+    'vehicle-miner-mk2-0-_-signal'
+}
 
-local SIGNAL_NAMES = {'vehicle-hauler-0-_-signal', 'vehicle-miner-0-_-signal'}
+-- if last quantity was 0 then move to next signal, else increment unitIndex
+if red['signal-count'] == 0 or (var.unitIndex or 0) >= red['signal-count'] then
+    -- increment signalIndex
+    var.signalIndex = (var.signalIndex or 0) % #SIGNAL_NAMES + 1
 
--- quantity
-local N = math.max(red['signal-count'], 1)
-
-local signal = SIGNAL_NAMES[math.min(var.signalIndex or 1, #SIGNAL_NAMES)]
-
--- increment value
-var.index = (var.index or 0) % N + 1
-out = {[signal] = var.index}
-
--- next signal if last index met
-if var.index >= N then
-    var.signalIndex = (var.signalIndex or 1) % #SIGNAL_NAMES + 1;
-    var.index = 0;
+    -- reset unitIndex
+    var.unitIndex = 1
+else
+    -- increment unitIndex
+    var.unitIndex = (var.unitIndex) % red['signal-count'] + 1
 end
+
+-- send test signal
+out = {[SIGNAL_NAMES[var.signalIndex]] = var.unitIndex}
 
 delay = 999999999
 irq = 'signal-A'
